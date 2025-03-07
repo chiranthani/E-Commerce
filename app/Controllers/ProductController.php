c1b7c3c88b115305e8d208956755184a5c985b96
chiranthani <ganeesha.rathnayake@archnix.com>
2025-03-07 22:04:20 +0530

    started the home page and routers creation

---
+++ app/Controllers/ProductController.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Controllers;
+
+use App\Helpers\ResponseHelper;
+use App\Models\Product;
+use PDO;
+use Exception;
+
+class ProductController
+{
+    private Product $productModel;
+
+    public function __construct(PDO $db)
+    {
+        $this->productModel = new Product($db);
+    }
+
+    /**
+     * get product details
+     */
+    public function getProduct(int $id): void
+    {
+        try {
+            $product = $this->productModel->find($id);
+            if ($product) {
+                ResponseHelper::jsonResponse($product, 200);
+            } else {
+                ResponseHelper::jsonResponse(['error' => 'Product Not Found'], 404);
+            }
+        } catch (Exception $e) {
+            error_log("Product Retrieval Error: " . $e->getMessage());
+            ResponseHelper::jsonResponse(['error' => 'Internal Server Error'], 500);
+        }
+    }
+
+     /**
+     * get all products
+     */
+    public function getAllProduct(): void
+    {
+        try {
+            $products = $this->productModel->getAllProducts();
+            if ($products) {
+                ResponseHelper::jsonResponse($products, 200);
+            } else {
+                ResponseHelper::jsonResponse(['error' => 'Product Not Found'], 404);
+            }
+        } catch (Exception $e) {
+            error_log("Product Retrieval Error: " . $e->getMessage());
+            ResponseHelper::jsonResponse(['error' => 'Internal Server Error'], 500);
+        }
+    }
+}
